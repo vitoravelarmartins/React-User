246f93c8b34023edd401aeb4f20ac50e36717f38
Vitor Avelar Martins <vitor.martins@sou.unifeob.edu.br>
2019-09-20 14:46:06 -0300

    Acabado

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
-import React from 'react';
-import GitUser from './componets/GitUser';
-import './App.css';
+import React from "react";
 
-export default class App extends React.Component {
-  state = {
-    inputText: "",
-    username: ""
-  };
+import GithubUser from "./componets/GitUser";
 
-  handleUsernameChange = e => {
-    this.setState({ inputText: e.target.value }, () => {
-      console.log(this.state)
-    });
-  };
 
-  handleButtonClick = e => {
-    this.setState({ ...this.state, username: this.state.inputText }, () => {
-      console.log(this.state);
-    });
-  };
-  render() {
-    return (
-      <>
-        <input
-          type="text"
-          onChange={this.handleUsernameChange}
-          value={this.state.inputText}
-        />
-        <button onClick={this.handleButtonClick}>Atulizar</button>
-        <GitUser username={this.state.username} />
-      </>
+function App() {
 
-    );
-  }
+  return (
+    <>
+      <GithubUser username='vitoravelarmartins' />
+    </>
+  );
 }
 
+
+export default App;
 
 
--- src/componets/GitUser/index.js
+++ src/componets/GitUser/index.js
@@ -1,57 +1,117 @@
-import React, { Component } from 'react';
-import ReportDispatcher from 'jest-jasmine2/build/jasmine/ReportDispatcher';
+import React, { Component } from "react";
 
 
-export default class GitUser extends Component {
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+
+
+const useStyles = makeStyles(theme => ({
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
+
+
+export default class GithubUser extends Component {
     state = {
-        users: []
-
+        users: Object
     };
 
-    async atulizarUsers() {
-        let { username } = this.props;
 
-        if (!username) {
-            username = "vitoravelarmartins";
-        }
+    async componentDidMount() {
+        const resposta = await fetch(`https://api.github.com/users/${this.props.username}`)
+        const users = await resposta.json();
 
 
-        const response = await fetch(
-            `https://api.github.com/users/${username}`
-        );
-        const users = await response.json();
-        this.setState({ users });
-    }
+        this.setState({ users })
+        console.log(users)
 
-    componentDidMount() {
-        this.atulizarUsers();
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        this.atulizarUsers();
-        return true;
 
     }
+
 
     render() {
+
         const { users } = this.state;
+
         return (
-            <ul>
-                {users.map(user => {
-                    return (
-                        <li key={user.id}>
-                            <stron>{user.login}</stron>
-                            &nbsp;<a href={user.html_url}>{user.html_url}</a>
-                        </li>
+            <Card >
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="recipe" >
+                            V
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title={users.name}
+                    subheader={'@' + users.login}
+                />
+                <div>
+                    <img src={users.avatar_url}></img>
+                </div>
 
-                    );
-                })}
-            </ul>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {"Quantidade de Repositorios: " + users.public_repos}
+                        <br></br>
+                        {"Quantidade de Seguidores: " + users.followers}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+
+                </CardActions>
+
+            </Card>
         );
-
     }
+
+
+
+
 }
+
 
 
 
